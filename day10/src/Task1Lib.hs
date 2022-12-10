d136f6006aaf4e1cc24f0958516718feec89e7bd
functionalprogrammingftw <functionalprogrammingftw@proton.me>
2022-12-10 07:37:08 +0100

    Day 10

--- day10/src/Task1Lib.hs
+++ day10/src/Task1Lib.hs
@@ -7,105 +7,38 @@ import qualified Data.Map (Map, empty, lookup, insert, foldr)
 
 taskFunc :: [String] -> IO ()
 taskFunc inputLines = do
-    putStrLn "Input lines:"
-    print inputLines
+    putStrLn "Counts:"
+    let counts = parseInputLines inputLines
+    print counts
+    putStrLn "Sum counts:"
+    let sumCounts = calcSumCounts counts
+    print sumCounts
+    putStrLn "Signal strengths:"
+    let signalStrenths = findSignalStrengths [20, 60, 100, 140, 180, 220] sumCounts
+    print $ sum signalStrenths
 
-
-
-
-
-
-
-
-parseInputLines :: [String] -> [(Char, Int)]
+parseInputLines :: [String] -> [Int]
 parseInputLines [] = []
-parseInputLines (line:lines) = command:parseInputLines lines
-    where command = (head $ head splitted, readInt $ last splitted)
-          splitted = splitOn " " line
+parseInputLines (line:lines) = count:parseInputLines lines
+    where splitted = splitOn " " line
+          count
+            | head splitted == "noop" = 0
+            | otherwise = readInt $ last splitted
 
-calcUniqueTailPosList :: [Position] -> [(Int, Int)]
+findSignalStrengths :: [Int] -> [Int] -> [Int]
+findSignalStrengths [] sumCounts = []
+findSignalStrengths (x:xs) sumCounts = (x * (sumCounts !! (x - 1))):findSignalStrengths xs sumCounts
 
-calcUniqueTailPosList positions = nub $ map tailPos positions
 
-calcTailPosList :: [Position] -> [(Char, Int)] -> [Position]
+calcSumCounts :: [Int] -> [Int]
+calcSumCounts = reverse . subCalcSumCounts . reverse
 
-calcTailPosList = foldl calcTailPosListSingleCommand
-
-calcTailPosListSingleCommand :: [Position] -> (Char, Int) -> [Position]
-
-calcTailPosListSingleCommand positions (_, 0) = positions
-
-calcTailPosListSingleCommand (position:positions) ('R', count) =
-    calcTailPosListSingleCommand (newPosition:position:positions) ('R', count - 1)
-    where (Position (headX, headY) (tailX, tailY)) = position
-          newHeadX = headX + 1
-          newHeadY = headY
-          newHeadPos = (newHeadX, newHeadY)
-          tailShouldMove = calcTailShouldMove newHeadPos (tailX, tailY)
-          newTailX
-            | tailShouldMove = tailX + 1
-            | otherwise = tailX
-          newTailY
-            | tailShouldMove && tailY < headY = tailY + 1
-            | tailShouldMove && tailY > headY = tailY - 1
-            | otherwise = tailY
-          newTailPos = (newTailX, newTailY)
-          newPosition = Position newHeadPos newTailPos
-
-calcTailPosListSingleCommand (position:positions) ('L', count) =
-    calcTailPosListSingleCommand (newPosition:position:positions) ('L', count - 1)
-    where (Position (headX, headY) (tailX, tailY)) = position
-          newHeadX = headX - 1
-          newHeadY = headY
-          newHeadPos = (newHeadX, newHeadY)
-          tailShouldMove = calcTailShouldMove newHeadPos (tailX, tailY)
-          newTailX
-            | tailShouldMove = tailX - 1
-            | otherwise = tailX
-          newTailY
-            | tailShouldMove && tailY < headY = tailY + 1
-            | tailShouldMove && tailY > headY = tailY - 1
-            | otherwise = tailY
-          newTailPos = (newTailX, newTailY)
-          newPosition = Position newHeadPos newTailPos
-
-calcTailPosListSingleCommand (position:positions) ('D', count) =
-    calcTailPosListSingleCommand (newPosition:position:positions) ('D', count - 1)
-    where (Position (headX, headY) (tailX, tailY)) = position
-          newHeadX = headX
-          newHeadY = headY + 1
-          newHeadPos = (newHeadX, newHeadY)
-          tailShouldMove = calcTailShouldMove newHeadPos (tailX, tailY)
-          newTailX
-            | tailShouldMove && tailX < headX = tailX + 1
-            | tailShouldMove && tailX > headX = tailX - 1
-            | otherwise = tailX
-          newTailY
-            | tailShouldMove = tailY + 1
-            | otherwise = tailY
-          newTailPos = (newTailX, newTailY)
-          newPosition = Position newHeadPos newTailPos
-
-calcTailPosListSingleCommand (position:positions) ('U', count) =
-    calcTailPosListSingleCommand (newPosition:position:positions) ('U', count - 1)
-    where (Position (headX, headY) (tailX, tailY)) = position
-          newHeadX = headX
-          newHeadY = headY - 1
-          newHeadPos = (newHeadX, newHeadY)
-          tailShouldMove = calcTailShouldMove newHeadPos (tailX, tailY)
-          newTailX
-            | tailShouldMove && tailX < headX = tailX + 1
-            | tailShouldMove && tailX > headX = tailX - 1
-            | otherwise = tailX
-          newTailY
-            | tailShouldMove = tailY - 1
-            | otherwise = tailY
-          newTailPos = (newTailX, newTailY)
-          newPosition = Position newHeadPos newTailPos
-
-calcTailShouldMove :: (Int, Int) -> (Int, Int) -> Bool
-
-calcTailShouldMove (newHeadX, newHeadY) (tailX, tailY) = abs (newHeadX - tailX) > 1 || abs (newHeadY - tailY) > 1
-
-data Position = Position {headPos :: (Int, Int), tailPos :: (Int, Int)}
-    deriving (Show, Eq)
+subCalcSumCounts :: [Int] -> [Int]
+subCalcSumCounts [] = [1]
+subCalcSumCounts (0:cs) = prevSum:prevSums
+    where prevSum = head prevSums
+          prevSums = subCalcSumCounts cs
+subCalcSumCounts (c:cs) = newSum:prevSum:prevSums
+    where newSum = c + prevSum
+          prevSum = head prevSums
+          prevSums = subCalcSumCounts cs
--- day10/src/Task2Lib.hs
+++ day10/src/Task2Lib.hs
@@ -1,62 +1,62 @@
 module Task2Lib (taskFunc) where
 
 import Data.List.Split (splitOn)
-import UtilLib (every, readInt, countTrueGrid)
+import UtilLib (every, readInt, countTrueGrid, splitEvery)
 import Data.List (nub, stripPrefix, insert, intercalate)
 import qualified Data.Map (Map, empty, lookup, insert, foldr)
 
 taskFunc :: [String] -> IO ()
 taskFunc inputLines = do
-    putStrLn "Input lines:"
-    print inputLines
+    putStrLn "Counts:"
+    let counts = parseInputLines inputLines
+    print counts
+    putStrLn "Register X values:"
+    let registerXValues = calcRegisterXValues counts
+    print registerXValues
+    putStrLn "CRT pos and pixel poses:"
+    let crtPosAndPixelPoses = calcCrtPosAndPixelPoses $ calcPixelPoses registerXValues
+    print crtPosAndPixelPoses
+    putStrLn "Pixels:"
+    let pixels = calcPixels crtPosAndPixelPoses
+    printLines $ splitEvery 40 pixels
 
-parseInputLines :: [String] -> [(Char, Int)]
-
+parseInputLines :: [String] -> [Int]
 parseInputLines [] = []
+parseInputLines (line:lines) = count:parseInputLines lines
+    where splitted = splitOn " " line
+          count
+            | head splitted == "noop" = 0
+            | otherwise = readInt $ last splitted
 
-parseInputLines (line:lines) = command:parseInputLines lines
-    where command = (head $ head splitted, readInt $ last splitted)
-          splitted = splitOn " " line
+printLines :: [String] -> IO ()
+printLines [] = do
+    return ()
+printLines (str:strs) = do
+    print str
+    printLines strs
 
-ropeLength :: Int
-ropeLength = 10
+calcPixels :: [(Int, [Int])] -> String
+calcPixels [] = []
+calcPixels ((crtPos, pixelPoses):crtPosAndPixelPoses) = pixel:calcPixels crtPosAndPixelPoses
+    where pixel
+            | crtPos `elem` pixelPoses = 'X'
+            | otherwise = '.'
 
-initialPositions :: [Position]
-initialPositions = [Position $ replicate ropeLength (0, 0)]
+calcCrtPosAndPixelPoses :: [[Int]] -> [(Int, [Int])]
+calcCrtPosAndPixelPoses = zip [ x `mod` 40 | x <- [0..] ]
 
-calcUniqueTailPosList :: [Position] -> [(Int, Int)]
-calcUniqueTailPosList positions = nub $ map (\(Position list) -> list !! (ropeLength - 1)) positions
+calcPixelPoses :: [Int] -> [[Int]]
+calcPixelPoses = map (\x -> [x - 1, x, x + 1])
 
-calcPositions :: [Position] -> [(Char, Int)] -> [Position]
-calcPositions = foldl calcPositionsSingleCommand
+calcRegisterXValues :: [Int] -> [Int]
+calcRegisterXValues = reverse . subCalcSumCounts . reverse
 
-calcPositionsSingleCommand :: [Position] -> (Char, Int) -> [Position]
-calcPositionsSingleCommand positions (_, 0) = positions
-calcPositionsSingleCommand positions (move, count) = calcPositionsSingleCommand (calcPositionSingleMove (head positions) move:positions) (move, count - 1)
-
-calcPositionSingleMove :: Position -> Char -> Position
-calcPositionSingleMove (Position ((x, y):coords)) 'R' = calcPositionConsequencesSingleMove (Position ((x + 1, y):coords))
-calcPositionSingleMove (Position ((x, y):coords)) 'L' = calcPositionConsequencesSingleMove (Position ((x - 1, y):coords))
-calcPositionSingleMove (Position ((x, y):coords)) 'D' = calcPositionConsequencesSingleMove (Position ((x, y + 1):coords))
-calcPositionSingleMove (Position ((x, y):coords)) 'U' = calcPositionConsequencesSingleMove (Position ((x, y - 1):coords))
-
-calcPositionConsequencesSingleMove :: Position -> Position
-calcPositionConsequencesSingleMove (Position [updatedCoord]) = Position [updatedCoord]
-calcPositionConsequencesSingleMove (Position (updatedCoord:nextCoord:coords)) = Position (updatedCoord:updatedCoords)
-    where updatedNextCoord = calcUpdatedNextCoord updatedCoord nextCoord
-          Position updatedCoords = calcPositionConsequencesSingleMove (Position (updatedNextCoord:coords))
-
-calcUpdatedNextCoord :: (Int, Int) -> (Int, Int) -> (Int, Int)
-calcUpdatedNextCoord (updatedX, updatedY) (nextX, nextY)
-    | updatedX - nextX > 1 = (nextX + 1, calcCalibratedNextOther updatedY nextY)
-    | updatedX - nextX < -1 = (nextX - 1, calcCalibratedNextOther updatedY nextY)
-    | updatedY - nextY > 1 = (calcCalibratedNextOther updatedX nextX, nextY + 1)
-    | updatedY - nextY < -1 = (calcCalibratedNextOther updatedX nextX, nextY - 1)
-    | otherwise = (nextX, nextY)
-    where calcCalibratedNextOther updated next
-            | updated > next = next + 1
-            | updated < next = next - 1
-            | otherwise = next
-
-newtype Position = Position [(Int, Int)]
-    deriving (Show, Eq)
+subCalcSumCounts :: [Int] -> [Int]
+subCalcSumCounts [] = [1]
+subCalcSumCounts (0:cs) = prevSum:prevSums
+    where prevSum = head prevSums
+          prevSums = subCalcSumCounts cs
+subCalcSumCounts (c:cs) = newSum:prevSum:prevSums
+    where newSum = c + prevSum
+          prevSum = head prevSums
+          prevSums = subCalcSumCounts cs
