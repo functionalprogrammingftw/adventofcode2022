4d1b30d0ec78193eaecb3905fac62debfcae8b21
functionalprogrammingftw <functionalprogrammingftw@proton.me>
2022-12-13 15:21:56 +0100

    Day 14 init

---
+++ day14/src/Task1Lib.hs
@@ -0,0 +1,80 @@
+module Task1Lib (taskFunc) where
+
+import Data.List.Split (splitOn, chunk)
+import UtilLib (every, readInt, countTrueGrid, replaceNth)
+import Data.List (nub, stripPrefix, insert, intercalate, elemIndex)
+import qualified Data.Map (Map, empty, lookup, insert, foldr)
+import Data.Char (ord)
+import Data.Maybe (fromJust)
+
+taskFunc :: [String] -> IO ()
+taskFunc inputLines = do
+    -- putStrLn "Input lines:"
+    -- print inputLines
+    -- putStrLn "Packet data pairs:"
+    let packetDataPairs = parseInputLines inputLines
+    -- printPairs packetDataPairs
+    -- putStrLn "Compare list:"
+    let compareList = comparePacketDataPairs packetDataPairs
+    -- print compareList
+    putStrLn "Count:"
+    print $ sumTrueIndices compareList
+
+data PacketData = PDInt Int | PDList [PacketData] deriving (Show, Eq)
+
+printPairs :: [(PacketData, PacketData)] -> IO ()
+printPairs [pair] = print pair
+printPairs (pair:pairs) = do
+    print pair
+    printPairs pairs
+
+sumTrueIndices :: [Bool] -> Int
+sumTrueIndices bools = sum [ index | (index, bool) <- zip [1..] bools, bool ]
+
+comparePacketDataPairs :: [(PacketData, PacketData)] -> [Bool]
+comparePacketDataPairs = map (fromJust . comparePacketDataPair)
+
+comparePacketDataPair :: (PacketData, PacketData) -> Maybe Bool
+comparePacketDataPair (PDInt leftInt, PDInt rightInt)
+    | leftInt < rightInt = Just True
+    | leftInt > rightInt = Just False
+    | otherwise = Nothing
+comparePacketDataPair (PDList [], PDList []) = Nothing
+comparePacketDataPair (PDList [], PDList (_:_)) = Just True
+comparePacketDataPair (PDList (_:_), PDList []) = Just False
+comparePacketDataPair (PDList (leftPacketData:leftRestList), PDList (rightPacketData:rightRestList)) =
+    case comparePacketDataPair (leftPacketData, rightPacketData) of
+        Nothing -> comparePacketDataPair (PDList leftRestList, PDList rightRestList)
+        justB -> justB
+comparePacketDataPair (PDList leftList, PDInt rightInt) = comparePacketDataPair (PDList leftList, PDList [PDInt rightInt])
+comparePacketDataPair (PDInt leftInt, PDList rightList) = comparePacketDataPair (PDList [PDInt leftInt], PDList rightList)
+
+parseInputLines :: [String] -> [(PacketData, PacketData)]
+parseInputLines inputLines = map parseInputLinePair inputLinePairs
+    where inputLinePairs = splitOn [""] inputLines
+
+parseInputLinePair :: [String] -> (PacketData, PacketData)
+parseInputLinePair [line1, line2] = (parseInputLine line1, parseInputLine line2)
+
+parseInputLine :: String -> PacketData
+parseInputLine ('[':str) = packetData
+    where (_, packetData) = parsePacketData ('[':str)
+
+parsePacketData :: String -> (String, PacketData)
+parsePacketData str = case str of
+    ('[':restStr) -> parsePDList restStr (PDList [])
+    (c:_) -> parsePDInt str (PDInt 0)
+
+parsePDList :: String -> PacketData -> (String, PacketData)
+parsePDList str currPdList = case (str, currPdList) of
+    (']':restStr, _) -> (restStr, currPdList)
+    (',':restStr, PDList currList) -> parsePDList restStr2 (PDList (currList ++ [packetData]))
+        where (restStr2, packetData) = parsePacketData restStr
+    (_, PDList currList) -> parsePDList restStr2 (PDList (currList ++ [packetData]))
+        where (restStr2, packetData) = parsePacketData str
+
+parsePDInt :: String -> PacketData -> (String, PacketData)
+parsePDInt str currPdInt = case (str, currPdInt) of
+    (']':_, _) -> (str, currPdInt)
+    (',':_, _) -> (str, currPdInt)
+    (c:restStr, PDInt currInt) -> parsePDInt restStr (PDInt (currInt * 10 + UtilLib.readInt [c]))
---
+++ day14/src/UtilLib.hs
@@ -0,0 +1,81 @@
+module UtilLib (
+    readInt,
+    readInteger,
+    maximum3,
+    sumTupleElements,
+    splitEvery,
+    tuplify2,
+    elems,
+    elemsOverlap,
+    every,
+    countTrue,
+    countTrueGrid,
+    replaceNth,
+    fpow,
+    primeFactors
+) where
+
+readInt :: String -> Int
+readInt = read
+
+readInteger :: String -> Integer
+readInteger = read
+
+maximum3 :: [Int] -> (Int, Int, Int)
+maximum3 = foldl maximum3Fold (0, 0, 0)
+
+maximum3Fold :: (Int, Int, Int) -> Int -> (Int, Int, Int)
+maximum3Fold (fstMax, sndMax, trdMax) elm
+    | elm > fstMax = (elm, fstMax, sndMax)
+    | elm > sndMax = (fstMax, elm, sndMax)
+    | elm > trdMax = (fstMax, sndMax, elm)
+    | otherwise = (fstMax, sndMax, trdMax)
+
+sumTupleElements :: (Int, Int, Int) -> Int
+sumTupleElements (x, y, z) = x + y + z
+
+splitEvery _ [] = []
+splitEvery n list = first : splitEvery n rest
+    where
+        (first, rest) = splitAt n list
+
+tuplify2 :: [a] -> (a,a)
+tuplify2 [x,y] = (x,y)
+
+elems :: Eq a => [a] -> [a] -> Bool
+elems [] _ = True
+elems (x:xs) ys
+    | x `elem` ys = elems xs ys
+    | otherwise = False
+
+elemsOverlap :: Eq a => [a] -> [a] -> Bool
+elemsOverlap [] _ = False
+elemsOverlap (x:xs) ys
+    | x `elem` ys = True
+    | otherwise = elemsOverlap xs ys
+
+every :: Int -> [a] -> [a]
+every n xs = case drop (n-1) xs of
+              y : ys -> y : every n ys
+              [] -> []
+
+countTrue :: [Bool] -> Int
+countTrue list = sum $ map fromEnum list
+
+countTrueGrid :: [[Bool]] -> Int
+countTrueGrid grid = sum $ map countTrue grid
+
+replaceNth :: Int -> a -> [a] -> [a]
+replaceNth _ _ [] = []
+replaceNth n newVal (x:xs)
+    | n == 0 = newVal:xs
+    | otherwise = x:replaceNth (n-1) newVal xs
+
+fpow 0 f = id
+fpow n f = f . fpow (n-1) f
+
+primeFactors n =
+  case factors of
+    [] -> [n]
+    _  -> factors ++ primeFactors (n `div` (head factors))
+  where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
