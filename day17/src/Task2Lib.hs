31655a04f09f9ec1b98a237ba8db62637b87f0cb
functionalprogrammingftw <functionalprogrammingftw@proton.me>
2022-12-20 22:24:08 +0100

    day 17

--- day17/src/Task2Lib.hs
+++ day17/src/Task2Lib.hs
@@ -11,14 +11,30 @@ import UtilLib (countTrueGrid, every, readInt, replaceNth)
 
 taskFunc :: [String] -> IO ()
 taskFunc inputLines = do
-  putStrLn "Jet patterns:"
   let jetPatterns = parseInputLines inputLines
-  print $ take 100 jetPatterns
-  -- putStrLn "End position:"
-  handleDropsAndPrint 1000 1000 jetPatterns rocks [] 0
-  -- printPosition position
+  putStrLn "End position:"
+  let (newPosition, newJetPatterns) = handleMultipleRocksDrops 560 [] jetPatterns
+  printPosition newPosition
+  putStrLn "Length position:"
+  let lengthNewPosition = length newPosition
+  print $ lengthNewPosition
+  putStrLn "Find repeat:"
+  let (repeatPosition, count) = findRepeat newJetPatterns newPosition
+  putStrLn "Repeat count:"
+  let repeatCount = count * 5
+  print repeatCount
+  putStrLn "Length of repeat:"
+  let repeatLength = length repeatPosition - lengthNewPosition
+  print repeatLength
+  putStrLn "Modulus:"
+  let modulus = (1000000000000 - 560 * 5) `mod` repeatCount
+  print modulus
+  putStrLn "Result:"
+  let result = (1000000000000 - 560 * 5) `div` repeatCount * repeatLength + lengthNewPosition
+  print result
 
-parseInputLines :: [String] -> String
+
+parseInputLines :: [String] -> [Char]
 parseInputLines = cycle . head
 
 type Position = [[Bool]]
@@ -30,17 +46,16 @@ initialPosition = []
 
 rocks :: [Rock]
 rocks =
-  cycle
-    [ [(2, 0), (3, 0), (4, 0), (5, 0)],
-      [(3, 0), (2, 1), (3, 1), (4, 1), (3, 2)],
-      [(4, 0), (4, 1), (2, 2), (3, 2), (4, 2)],
-      [(2, 0), (2, 1), (2, 2), (2, 3)],
-      [(2, 0), (3, 0), (2, 1), (3, 1)]
-    ]
+  [ [(2, 0), (3, 0), (4, 0), (5, 0)],
+    [(3, 0), (2, 1), (3, 1), (4, 1), (3, 2)],
+    [(4, 0), (4, 1), (2, 2), (3, 2), (4, 2)],
+    [(2, 0), (2, 1), (2, 2), (2, 3)],
+    [(2, 0), (3, 0), (2, 1), (3, 1)]
+  ]
 
 printPosition :: Position -> IO ()
 printPosition [] = return ()
-printPosition (row:rows) = do
+printPosition (row : rows) = do
   let rowStr = map (\b -> if b then '#' else '.') row
   putStrLn rowStr
   printPosition rows
@@ -48,40 +63,58 @@ printPosition (row:rows) = do
 positionWidth :: Int
 positionWidth = 7
 
-handleDropsAndPrint :: Int -> Int -> [Char] -> [Rock] -> Position -> Int -> IO ()
-handleDropsAndPrint 0 _ _ _ _ _ = return ()
-handleDropsAndPrint outerStepCount innerStepCount jetPatterns rocks position rowsRemoved = do
-  print $ show outerStepCount ++ " " ++ show (length newPosition + totalRowsRemoved)
-  handleDropsAndPrint (outerStepCount - 1) innerStepCount newJetPatterns newRocks newPosition totalRowsRemoved
-  where (newPosition, newJetPatterns, newRocks, newRowsRemoved) = handleDrops innerStepCount jetPatterns rocks position
-        totalRowsRemoved = rowsRemoved + newRowsRemoved
-
-handleDrops :: Int -> [Char] -> [Rock] -> Position -> (Position, [Char], [Rock], Int)
-handleDrops stepCount jetPatterns rocks position = foldl handleDrop (position, jetPatterns, rocks, 0) [1 .. stepCount]
-
-handleDrop :: (Position, [Char], [Rock], Int) -> Int -> (Position, [Char], [Rock], Int)
-handleDrop (position, jetPatterns, rocks, rowsRemoved) step = (newPosition, newJetPatterns, newRocks, rowsRemoved + newRowsRemoved)
+findRepeat :: [Char] -> Position -> (Position, Int)
+findRepeat jetPatterns position = findRepeatSub position newJetPatterns newPosition 1
   where
-    newRocks = tail rocks
-    nextRock = head rocks
-    updatedPosition = replicate (calcRockHeight nextRock + 3) (replicate positionWidth False) ++ position
-    (newPosition, newJetPatterns, newRowsRemoved) = performDrop updatedPosition jetPatterns nextRock
+    (newPosition, newJetPatterns) = handleDrops position jetPatterns rocks
+
+findRepeatSub :: Position -> [Char] -> Position -> Int -> (Position, Int)
+findRepeatSub firstPosition jetPatterns position count = do
+  if take 40 firstPosition == take 40 position
+    then (position, count)
+    else findRepeatSub firstPosition newJetPatterns newPosition (count + 1)
+  where
+    (newPosition, newJetPatterns) = handleDrops position jetPatterns rocks
+
+findStr :: String -> String -> Maybe Int
+findStr pat str = findStrHelp pat str 0
+  where
+    findStrHelp _ [] _ = Nothing
+    findStrHelp pat s@(x : xs) n
+      | pat == take (length pat) s = Just n
+      | otherwise = findStrHelp pat xs (n + 1)
+
+handleMultipleRocksDrops :: Int -> Position -> [Char] -> (Position, [Char])
+handleMultipleRocksDrops count position jetPatterns =
+  if count == 0
+    then (position, jetPatterns)
+    else handleMultipleRocksDrops (count - 1) newPosition newJetPatterns
+  where
+    (newPosition, newJetPatterns) = handleDrops position jetPatterns rocks
+
+handleDrops :: Position -> [Char] -> [Rock] -> (Position, [Char])
+handleDrops position jetPatterns = foldl handleDrop (position, jetPatterns)
+
+handleDrop :: (Position, [Char]) -> Rock -> (Position, [Char])
+handleDrop (position, jetPatterns) rock = (newPosition, newJetPatterns)
+  where
+    updatedPosition = replicate (calcRockHeight rock + 3) (replicate positionWidth False) ++ position
+    (newPosition, newJetPatterns) = performDrop updatedPosition jetPatterns rock
 
 calcRockHeight :: Rock -> Int
 calcRockHeight rock = maximum ys - minimum ys + 1
   where
     ys = map snd rock
 
-performDrop :: Position -> [Char] -> Rock -> (Position, [Char], Int)
+performDrop :: Position -> [Char] -> Rock -> (Position, [Char])
 performDrop position (jetPattern : jetPatterns) rock =
   if rockFits position droppedRock
     then performDrop position jetPatterns droppedRock
-    else (topOfNewPositionWithRock, jetPatterns, rowsRemoved)
+    else (newPositionWithRock, jetPatterns)
   where
     pushedRock = if jetPattern == '<' then moveRockLeft position rock else moveRockRight position rock
     droppedRock = map (\(x, y) -> (x, y + 1)) pushedRock
     newPositionWithRock = removeEmptyRowsFromTop $ updatePosition position pushedRock
-    (topOfNewPositionWithRock, rowsRemoved) = removeRowsFromBottom newPositionWithRock
 
 moveRockLeft :: Position -> Rock -> Rock
 moveRockLeft position rock = if rockFits position movedRock then movedRock else rock
@@ -98,15 +131,12 @@ rockFits position rock = not (any (\(x, y) -> x < 0 || x >= positionWidth || y >
 
 updatePosition :: Position -> Rock -> Position
 updatePosition position [] = position
-updatePosition position ((x, y):coords) = updatePosition newPosition coords
-  where newPosition = take y position ++ updatedRow : drop (y + 1) position
-        row = position !! y
-        updatedRow = take x row ++ True : drop (x + 1) row
+updatePosition position ((x, y) : coords) = updatePosition newPosition coords
+  where
+    newPosition = take y position ++ updatedRow : drop (y + 1) position
+    row = position !! y
+    updatedRow = take x row ++ True : drop (x + 1) row
 
 removeEmptyRowsFromTop :: Position -> Position
 removeEmptyRowsFromTop [] = []
-removeEmptyRowsFromTop (row:rows) = if True `notElem` row then removeEmptyRowsFromTop rows else row:rows
-
-removeRowsFromBottom :: Position -> (Position, Int)
-removeRowsFromBottom position = if positionLength > 100 then (take 100 position, positionLength - 100) else (position, 0)
-  where positionLength = length position
+removeEmptyRowsFromTop (row : rows) = if True `notElem` row then removeEmptyRowsFromTop rows else row : rows
