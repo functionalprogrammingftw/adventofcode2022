e51da15f88222449867e6e77429d8eb0dd61c933
functionalprogrammingftw <functionalprogrammingftw@proton.me>
2022-12-21 11:04:03 +0100

    day 19

--- day19/src/Task1Lib.hs
+++ day19/src/Task1Lib.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Task1Lib (taskFunc) where
 
 import Control.Monad.State (MonadState (get, put), State)
 import Data.Char (ord)
 import Data.List (elemIndex, insert, intercalate, nub, stripPrefix)
-import Data.List.Split (chunk, splitOn)
+import Data.List.Split (chunk, split, splitOn)
 import qualified Data.Map (Map, empty, insert, lookup)
 import Data.Maybe (fromJust)
 import qualified Data.Set (Set, delete, empty, fromList, insert, isSubsetOf, singleton, union)
@@ -11,37 +13,80 @@ import UtilLib (countTrueGrid, every, readInt, replaceNth)
 
 taskFunc :: [String] -> IO ()
 taskFunc inputLines = do
-  putStrLn "Input lines:"
-  print inputLines
+  putStrLn "Blueprints:"
+  let blueprints = parseInputLines inputLines
+  print blueprints
 
-data RobotPrice = RobotPrice {
-  orePrice :: Int,
-  clayPrice :: Int,
-  obsidianPrice :: Int
-} deriving (Show, Eq)
+parseInputLines :: [String] -> [Blueprint]
+parseInputLines = map parseInputLine
 
-data Blueprint = Blueprint {
-  oreRobotPrice :: RobotPrice,
-  clayRobotPrice :: RobotPrice,
-  obsidianRobotPrice :: RobotPrice,
-  geodeRobotPrice :: RobotPrice
-} deriving (Show, Eq)
+parseInputLine :: String -> Blueprint
+parseInputLine str = Blueprint {blueprintId, oreRobotPrice, clayRobotPrice, obsidianRobotPrice, geodeRobotPrice}
+  where
+    [startStr, endStr] = splitOn ": " str
+    blueprintId = UtilLib.readInt $ drop 10 startStr
+    [oreRobotPriceStr, clayRobotPriceStr, obsidianRobotPriceStr, geodeRobotPriceStr] = splitOn ". " str
+    oreRobotPrice =
+      RobotPrice
+        { orePrice = UtilLib.readInt $ head $ splitOn " " (drop 21 oreRobotPriceStr),
+          clayPrice = 0,
+          obsidianPrice = 0
+        }
+    clayRobotPrice =
+      RobotPrice
+        { orePrice = UtilLib.readInt $ head $ splitOn " " (drop 22 clayRobotPriceStr),
+          clayPrice = 0,
+          obsidianPrice = 0
+        }
+    obsidianRobotPrice =
+      RobotPrice
+        { orePrice = UtilLib.readInt $ head $ splitOn " " (drop 26 obsidianRobotPriceStr),
+          clayPrice = 0,
+          obsidianPrice = 0
+        }
+    geodeRobotPrice =
+      RobotPrice
+        { orePrice = UtilLib.readInt $ head $ splitOn " " (drop 23 geodeRobotPriceStr),
+          clayPrice = 0,
+          obsidianPrice = 0
+        }
 
-data RobotConfig = RobotConfig {
-  oreRobots :: Int,
-  clayRobots :: Int,
-  obsidianRobots :: Int,
-  geodeRobots :: Int
-} deriving (Show, Eq)
+-- Model
 
-data Resources = Resources {
-  ore :: Int,
-  clay :: Int,
-  obsidian :: Int,
-  geode :: Int
-} deriving (Show, Eq)
+data RobotPrice = RobotPrice
+  { orePrice :: Int,
+    clayPrice :: Int,
+    obsidianPrice :: Int
+  }
+  deriving (Show, Eq)
 
-data Inventory = Inventory {
-  resources :: Resources,
-  robotConfig :: RobotConfig
-} deriving (Show, Eq)
+data Blueprint = Blueprint
+  { blueprintId :: Int,
+    oreRobotPrice :: RobotPrice,
+    clayRobotPrice :: RobotPrice,
+    obsidianRobotPrice :: RobotPrice,
+    geodeRobotPrice :: RobotPrice
+  }
+  deriving (Show, Eq)
+
+data RobotConfig = RobotConfig
+  { oreRobots :: Int,
+    clayRobots :: Int,
+    obsidianRobots :: Int,
+    geodeRobots :: Int
+  }
+  deriving (Show, Eq)
+
+data Resources = Resources
+  { ore :: Int,
+    clay :: Int,
+    obsidian :: Int,
+    geode :: Int
+  }
+  deriving (Show, Eq)
+
+data Inventory = Inventory
+  { resources :: Resources,
+    robotConfig :: RobotConfig
+  }
+  deriving (Show, Eq)
