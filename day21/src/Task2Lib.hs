cfcae81fdf6ac22ca8f0487e66f47345effa637b
functionalprogrammingftw <functionalprogrammingftw@proton.me>
2022-12-30 09:54:01 +0100

    day 21

--- day21/src/Task2Lib.hs
+++ day21/src/Task2Lib.hs
@@ -11,67 +11,11 @@ import UtilLib (anyIndexed, countTrueGrid, every, filterIndexed, readInt, replac
 
 taskFunc :: [String] -> IO ()
 taskFunc inputLines = do
-  let numbers = parseInputLines inputLines
-  putStrLn "Numbers:"
-  print numbers
-  putStrLn "Numbers length:"
-  print $ length numbers
-  -- let rearrangedNumbers1 = rearrangeNumber numbers (numbers !! 0) 
-  -- putStrLn "Rearranged numbers:"
-  -- print rearrangedNumbers1
-  -- let rearrangedNumbers2 = rearrangeNumber rearrangedNumbers1 (numbers !! 1) 
-  -- putStrLn "Rearranged numbers:"
-  -- print rearrangedNumbers2
-  -- let rearrangedNumbers3 = rearrangeNumber rearrangedNumbers2 (numbers !! 2) 
-  -- putStrLn "Rearranged numbers:"
-  -- print rearrangedNumbers3
-  let rearrangedNumbers = indexAndRearrangeNumbers numbers 10
-  putStrLn "Rearranged numbers:"
-  print rearrangedNumbers
-  let coordinates@(x, y, z) = findCoordinates rearrangedNumbers 
-  putStrLn "Coordinates:"
-  print coordinates
-  putStrLn "Sum:"
-  print (x + y + z)
+  putStrLn "Input lines:"
+  print inputLines
 
 parseInputLines :: [String] -> [Int]
 parseInputLines = map parseInputLine
 
 parseInputLine :: String -> Int
-parseInputLine str = 811589153 * UtilLib.readInt str
-
-findCoordinates :: [(Int, Int)] -> (Int, Int, Int)
-findCoordinates indexedNumbers = (x, y, z)
-  where numbers = map fst indexedNumbers
-        numbersLength = length numbers
-        zeroIndex = fromJust $ elemIndex 0 numbers
-        x = numbers !! ((zeroIndex + 1000) `mod` numbersLength)
-        y = numbers !! ((zeroIndex + 2000) `mod` numbersLength)
-        z = numbers !! ((zeroIndex + 3000) `mod` numbersLength)
-
-indexAndRearrangeNumbers :: [Int] -> Int -> [(Int, Int)]
-indexAndRearrangeNumbers numbers = rearrangeNumbers indexedNumbers indexedNumbers
-  where indexedNumbers = zip numbers [0..]
-
-rearrangeNumbers :: [(Int, Int)] -> [(Int, Int)] -> Int -> [(Int, Int)]
-rearrangeNumbers indexedNumbers originalIndexedNumbers count = case count of
-  1 -> rearrangedOnce
-  _ -> rearrangeNumbers rearrangedOnce originalIndexedNumbers (count - 1)
-  where rearrangedOnce = foldl rearrangeNumber indexedNumbers originalIndexedNumbers
-
-
-rearrangeNumber :: [(Int, Int)] -> (Int, Int) -> [(Int, Int)]
-rearrangeNumber indexedNumbers indexedNumber
-  | fst indexedNumber == 0 = indexedNumbers
-  | otherwise = rearrangeNumberExceptZero indexedNumbers indexedNumber
-
-rearrangeNumberExceptZero :: [(Int, Int)] -> (Int, Int) -> [(Int, Int)]
-rearrangeNumberExceptZero indexedNumbers indexedNumber = take newNumberIdx numbersRemoved ++ [indexedNumber] ++ drop newNumberIdx numbersRemoved
-  where numbersLength = length indexedNumbers
-        numberIdx = fromJust $ elemIndex indexedNumber indexedNumbers
-        potentialNumberIdx = numberIdx + (fst indexedNumber `mod` (numbersLength - 1))
-        newNumberIdx
-          | potentialNumberIdx <= 0 = potentialNumberIdx + (numbersLength - 1)
-          | potentialNumberIdx >= numbersLength - 1 = potentialNumberIdx `mod` (numbersLength - 1)
-          | otherwise = potentialNumberIdx
-        numbersRemoved = take numberIdx indexedNumbers ++ drop (numberIdx + 1) indexedNumbers
+parseInputLine = UtilLib.readInt
